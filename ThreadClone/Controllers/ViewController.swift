0d805d4c51999f019039074e4e931862da698e69
shagaranasution <shagaranst@gmail.com>
2023-10-17 11:17:53 +0700

    Initial commit

---
+++ ThreadClone/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadClone
+//
+//  Created by Shagara F Nasution on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
